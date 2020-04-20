ec513773f31cf83e4ab8a652017fadc86d55e12c
buhtla85 <tamara.naunovic@gmail.com>
2020-04-20 19:19:15 +0200

    basic react router setup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,36 +1,13 @@
 import * as React from 'react';
+import { HomePage } from "./pages/Home";
+import { PhotosPage } from "./pages/Photos";
+import { Switch, Route } from "react-router-dom";
 
-class App extends React.Component<IAppProps, IAppState> {
-	constructor(props: IAppProps) {
-		super(props);
-		this.state = {
-			name: null
-		};
-	}
-
-	async componentDidMount() {
-		try {
-			let r = await fetch('/api/hello');
-			let name = await r.json();
-			this.setState({ name });
-		} catch (error) {
-			console.log(error);
-		}
-	}
-
-	render() {
-		return (
-			<main className="container my-5">
-				<h1 className="text-primary text-center">Hello {this.state.name}!</h1>
-			</main>
-		);
-	}
+export const App = (): JSX.Element => {
+	return (
+		<Switch>
+			<Route exact path="/" component={HomePage}/>
+			<Route exact path="/photos/" component={PhotosPage}/>
+		</Switch>
+	)
 }
-
-export interface IAppProps {}
-
-export interface IAppState {
-	name: string;
-}
-
-export default App;
