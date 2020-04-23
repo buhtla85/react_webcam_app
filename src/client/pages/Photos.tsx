76b7d42b0ce302aba9a7126ba9d227133564b989
buhtla85 <tamara.naunovic@gmail.com>
2020-04-23 15:40:11 +0200

    added display entries component

--- src/client/pages/Photos.tsx
+++ src/client/pages/Photos.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import { Header } from "../Header";
+import { DisplayEntries } from "../DisplayEntries";
 
 export const PhotosPage = (): JSX.Element => {
     return (
         <div className="text-center">
             <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
                 <Header />
+                <DisplayEntries />
             </div>
         </div>
     )
