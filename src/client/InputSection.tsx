f839e34b4b6ca39a34ca157e566b77154f543ce6
buhtla85 <tamara.naunovic@gmail.com>
2020-04-23 14:29:45 +0200

    added error alert

--- src/client/InputSection.tsx
+++ src/client/InputSection.tsx
@@ -31,25 +31,48 @@ export class InputSection extends React.Component<{}, IState> {
         this.setState({imgSrc: scrShoot});
     }
 
+    submitValues = async (e: React.ChangeEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (this.state.imgSrc.length < 1 || this.state.mood.length < 1 ) {
+            this.setState({errMsg: "Please make sure to enter a value or to click a capture button."})
+            setTimeout(() => {
+                this.setState({errMsg: ""})
+            }, 5000)
+        }
+        const { imgSrc, mood } = this.state;
+        const postVals = { imgSrc, mood }
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(postVals)
+        }
+        const response = await fetch("http://localhost:3000/", options);
+        const json = await response.json();
+        console.log(json);
+    }
+
 
     render () {
         return (
             <InnerContainer>
-                <h1 className="cover-heading">Take a photo app.</h1>
+                <h1 className="cover-heading">Record this moment...</h1>
                 <p className="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, sit.</p>
-                <form action="">
+                {this.state.errMsg.length === 0 ? "" : <div className="alert alert-danger" role="alert">{this.state.errMsg}</div>}
+                <form action="" onSubmit={this.submitValues}>
                     <div>
-                        <Webcam ref={this.myRef as any} videoConstraints={videoConstraints} audio={false} height={350} screenshotFormat="image/jpeg" width={350} />
+                        <Webcam ref={this.myRef as any} videoConstraints={videoConstraints} audio={false} height={300} screenshotFormat="image/jpeg" width={288} />
                         <p className="lead">
                             <button className="btn btn-secondary" type="button" onClick={this.capturePhoto}>Capture photo</button>
                         </p>
                     </div>
                     <label htmlFor="myInput">Enter your mood!</label>
                     <div>
-                        <input type="text" name="mood" value={this.state.mood} id="myInput" placeholder="Your mood goes here..." onChange={this.handleMoodInput} />
+                        <input type="text" name="mood" value={this.state.mood} id="myInput" onChange={this.handleMoodInput} />
                     </div>
                     <p className="lead">
-                        <button className="btn btn-lg btn-primary" type="submit">Submit!</button>
+                        <button className="btn btn-lg btn-primary mt-2" type="submit">Submit!</button>
                     </p>
                 </form>
             </InnerContainer>
