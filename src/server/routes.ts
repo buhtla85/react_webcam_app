79caaa54e3600f2e4132bb126c568c0dfb067150
buhtla85 <tamara.naunovic@gmail.com>
2020-04-23 13:12:09 +0200

    layout of the routes

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -2,8 +2,26 @@ import * as express from 'express';
 
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('World');
+interface ReqBodyValues {
+    mood: string,
+    imgSrc: string
+}
+
+//get all photos from db and display them on the photos page
+router.get('/photos', (req, res) => {
+    res.send("Get all entries.");
+});
+
+//delete a particular entry from db
+router.delete("/photos/:id", (req, res) => {
+    res.send("Delete a single entry");
+});
+
+//create a new entry on home page (inputssection component)
+router.post("/", (req, res) => {
+    res.send("New entry created");
+    console.log("I got a request!");
+    console.log(req.body);
 });
 
 export default router;
