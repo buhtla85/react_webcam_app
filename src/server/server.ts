2caeaecad509107983967c6ee8e12265d1aa1bf8
buhtla85 <tamara.naunovic@gmail.com>
2020-04-23 14:29:58 +0200

    minor changes

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,11 +8,12 @@ app.use(express.static('public'));
 app.use(express.json()); //parses incoming requests with JSON payloads and is based on body-parser. More info: https://expressjs.com/en/4x/api.html#express.json
 app.use(express.urlencoded({extended: false})); //parses incoming requests with urlencoded payloads and is based on body-parser. More info: https://expressjs.com/en/4x/api.html#express.json
 
+app.use(router);
+
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.use(router);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
