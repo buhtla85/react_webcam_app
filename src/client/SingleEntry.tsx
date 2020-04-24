9d8d0e7d19dd8863c88856aca3bad3bb0fe87b18
buhtla85 <tamara.naunovic@gmail.com>
2020-04-24 15:12:05 +0200

    preps for next iteration

--- src/client/SingleEntry.tsx
+++ src/client/SingleEntry.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
+import { IEntry } from "./DisplayEntries";
 
 const defaultIMG = "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"; 
 
+interface ISingleEntryProps {
+    singleEntry: IEntry,
+    index: number
+}
 
-export const SingleEntry = () => {
+export const SingleEntry = (props: ISingleEntryProps) => {
     return (
         <div className="col-sm-6 mb-3">
             <div className="card" >
-                <img src={defaultIMG} className="card-img-top" alt="..." />
+                <img src={props.singleEntry.photo} className="card-img-top" alt="..." />
                     <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <h5 className="card-title">Photo mood: </h5>
+                            <p className="card-text">{props.singleEntry.mood}</p>
                     </div>
             </div>
         </div>
