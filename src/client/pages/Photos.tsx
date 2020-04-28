fdeee60e9ed2d342f31d50d40eae1e5a7ae62d92
buhtla85 <tamara.naunovic@gmail.com>
2020-04-28 22:41:04 +0200

    changes for header props

--- src/client/pages/Photos.tsx
+++ src/client/pages/Photos.tsx
@@ -6,7 +6,7 @@ export const PhotosPage = (): JSX.Element => {
     return (
         <div className="text-center">
             <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-                <Header />
+                <Header customClass=" mb-5" />
                 <DisplayEntries />
             </div>
         </div>
