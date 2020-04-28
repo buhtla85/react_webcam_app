bbccdd9ec1d528af3f8a91adcc35103818707591
buhtla85 <tamara.naunovic@gmail.com>
2020-04-28 21:37:14 +0200

    added changes to props

--- src/client/SingleEntry.tsx
+++ src/client/SingleEntry.tsx
@@ -5,17 +5,19 @@ const defaultIMG = "https://www.salonlfc.com/wp-content/uploads/2018/01/image-no
 
 interface ISingleEntryProps {
     singleEntry: IEntry,
-    index: number
+    index: number,
+    removeEntry: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 export const SingleEntry = (props: ISingleEntryProps) => {
     return (
         <div className="col-sm-6 mb-3">
             <div className="card" >
-                <img src={props.singleEntry.photo} className="card-img-top" alt="..." />
+                <img src={props.singleEntry.imgSrc} className="card-img-top" alt="..." />
                     <div className="card-body">
                             <h5 className="card-title">Photo mood: </h5>
                             <p className="card-text">{props.singleEntry.mood}</p>
+                            <button type="button" className="btn btn-warning" onClick={props.removeEntry}>Delete entry</button>
                     </div>
             </div>
         </div>
