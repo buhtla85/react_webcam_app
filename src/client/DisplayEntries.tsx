7a3842e3ef158f49c002d0203afca59ec8a134a5
buhtla85 <tamara.naunovic@gmail.com>
2020-04-28 21:36:56 +0200

    added changes to state

--- src/client/DisplayEntries.tsx
+++ src/client/DisplayEntries.tsx
@@ -1,51 +1,63 @@
 import * as React from "react";
 import { InnerContainer } from "./utils/InnerCont";
 import { SingleEntry } from "./SingleEntry";
+import { Spinner } from "./Spinner";
 
 interface IStateEntries {
-    entries: IEntry[]
+    entries: IEntry[],
+    loading: boolean
 }
 
 export interface IEntry {
     mood: string,
-    photo: string
+    imgSrc: string,
+    _id: string,
+    date: string,
+    _v: number
 }
+
+interface ApiResponse {
+    count: number,
+    data: IEntry[],
+    success: boolean
+}
+
+//use the default _id from DB and use it in the app to delete entries
+//also, maybe add spinner component and loading value for the state
 
 export class DisplayEntries extends React.Component<{}, IStateEntries> {
     constructor(props: any) {
         super(props);
         this.state = {
-            entries: [{
-                mood: "not found",
-                photo: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
-            }, {
-                mood: "not found",
-                photo: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
-            }, {
-                mood: "not found",
-                photo: "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled-1150x647.png"
-            } ]
+            entries: [],
+            loading: false
         }
     }
 
     async componentDidMount() {
+        this.setState({loading: true});
         try {
             let hitMyApi = await fetch("/photos");
-            let entry = await hitMyApi.json();
-            console.log(entry);
+            let entry: ApiResponse = await hitMyApi.json();
+            this.setState({entries: entry.data, loading: false});
         } catch (error) {
             console.log(error);
         }
     }
 
+    removeEntry = (idx: number) => () => this.setState({entries: this.state.entries.filter((entry: IEntry, entryIdx: number) => idx !== entryIdx)});
+
     render() {
         return (
             <InnerContainer>
                 <div className="container">
+                    {this.state.loading ? <Spinner /> : ""}
                     <div className="row">
                         {this.state.entries.map((entry: IEntry, idx: number) => {
                             return (
-                                <SingleEntry singleEntry={entry} index={idx}/>
+                                <>
+                                    <SingleEntry singleEntry={entry} index={idx} removeEntry={this.removeEntry(idx)} key={idx}/>
+                                </>
                             )
                         })}
                     </div>
